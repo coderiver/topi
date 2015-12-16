07dd83d0ea807e72d648f442dc0bd98e46da397c
whats0n <mrdvv@mail.ru>
2015-12-16 14:02:07 +0200

    update

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
-	// go js
+	$('.js-menu-btn').click(function() {
+		$('.js-menu').toggleClass('is-active');
+		$('body').toggleClass('is-active');
+		return false;
+	});
+	$('.js-menu').each(function() {
+		$('body').click(function() {
+			$('.js-menu').removeClass('is-active');
+			$('body').removeClass('is-active');
+		});
+		$(this).click(function(event) {
+			event.stopPropagation();
+		});
+	});
 
 });
